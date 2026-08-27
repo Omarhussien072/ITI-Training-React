e0910236ead6d1f3c5920e896f2c12aa607056e3
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-08-27 14:06:27 +0300

    short activity

---
+++ Day 15/js/index.js
@@ -0,0 +1,13 @@
+
+// let asyncRequest = async () => {
+//     let res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//     if(res.ok){
+//         let resData = await res.json();
+//         console.log(resData);
+//     }
+// }
+
+// asyncRequest();
+
+
+
