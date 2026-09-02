a279ff6a91cbf12fc4d412ff2bc7653e84f9ffc3
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-09-02 10:06:46 +0300

    Day 19

--- Day 18/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <>
-            <div className="bg-dark p-2">
-                <h2 className="text-white text-center">Footer</h2>
-            </div>
-        </>
-    );
-}
---
+++ Day 19/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import NavbarStyles from './Navbar.module.css';
+export default function Navbar() {
+    return (
+        <>
+            <nav className={`navbar navbar-expand-lg navbar-light p-3 ${NavbarStyles.navbar}`}>
+                <NavLink className="navbar-brand  text-white" to="/">PetyStore</NavLink>
+                <button className="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse d-flex justify-content-end align-items-center me-5" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active d-flex gap-5 justify-content-center align-items-center">
+                        <NavLink className={`nav-link p-2 text-white`} to='/home'>Home</NavLink>
+                        <NavLink className={`nav-link p-2 text-white`} to='/about'>About</NavLink>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </>
+    );
+}
